b8abb2385b5b27a121e4075d69cf0cded5c28886
PipesAkaTubinhos <pipo.martins.27.06@gmail.com>
2022-01-25 17:46:49 +0000

    graph generation implemented

--- runGraph.sh
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/env bash
-
-valgrind --tool=callgrind  --callgrind-out-file=./callgrind.out ./main <input.txt
-
-gprof2dot -f callgrind callgrind.out | dot -Tsvg -o output.svg
-
-kcachegrind ./output.svg
--- runTests.sh
+++ runTests.sh
@@ -1,10 +1,9 @@
-#!/usr/bin/env bash
+#!/bin/sh
 
-./tests/tests/test.sh
-/usr/bin/env /usr/lib/jvm/java-16-openjdk-amd64/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /home/bernardo/.config/Code/User/workspaceStorage/3a0d3b06cd1ab5a09a9c85ed8b8ec12f/redhat.java/jdt_ws/Project-2-ASA-21_6f4fa2d5/bin test > results.txt
-cat results.txt
-
-./tests/smol_tests/test.sh
-cd /home/bernardo/Desktop/PROJASA2/Project-2-ASA-21 ; /usr/bin/env /usr/lib/jvm/java-16-openjdk-amd64/bin/java -XX:+ShowCodeDetailsInExceptionMessages -cp /home/bernardo/.config/Code/User/workspaceStorage/3a0d3b06cd1ab5a09a9c85ed8b8ec12f/redhat.java/jdt_ws/Project-2-ASA-21_6f4fa2d5/bin smol_test > results.txt
-
-cat results.txt
+echo "n1,n2,mean" > prob2.csv
+for i in {10000..40000..100}; do
+  ./randGeneoTree $i 0.7 9 > vector.txt
+  BENCH=$(hyperfine -w 4 -i -m 2 "./main < vector.txt" | grep -Po "([\\d\\.]+)\\s+[m]?s ±" | head -c-7)
+  echo "$i, "0.7",$BENCH" >> prob2.csv
+  echo "$i, $i, prob2"
+done
